160b3177c7d9ef3d1b386523a936f2a000b8fece
1547784788 <1547784788@qq.com>
2020-06-05 14:41:12 +0800

    2020.6.5

--- Kmeans/Kmeans.cpp
+++ Kmeans/Kmeans.cpp
@@ -2,10 +2,59 @@
 //
 
 #include <iostream>
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int createMaskByKmeans(cv::Mat src, cv::Mat & mask)
+{
+	if ((mask.type() != CV_8UC1)
+		|| (src.size() != mask.size())
+		) {
+		return 0;
+	}
+
+	int width = src.cols;
+	int height = src.rows;
+
+	int pixNum = width * height;
+	int clusterCount = 2;
+	Mat labels;
+	Mat centers;
+
+	//制作kmeans用的数据
+	Mat sampleData = src.reshape(3, pixNum);
+	Mat km_data;
+	sampleData.convertTo(km_data, CV_32F);
+
+	//执行kmeans
+	TermCriteria criteria = TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 0.1);
+	kmeans(km_data, clusterCount, labels, criteria, clusterCount, KMEANS_PP_CENTERS, centers);
+
+	//制作mask
+	uchar fg[2] = { 0,255 };
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			mask.at<uchar>(row, col) = fg[labels.at<int>(row*width + col)];
+		}
+	}
+
+	return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat srcMat = imread("E:\\课程\\大二下\\数字图像处理\\大象.jpg");
+	Mat mask = Mat::zeros(srcMat.size(), CV_8UC1);
+	createMaskByKmeans(srcMat, mask);
+	imshow("srcMat", srcMat);
+	imshow("mask", mask);
+	waitKey(0);
+
+
+
+	return 0;
+    //std::cout << "Hello World!\n";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
